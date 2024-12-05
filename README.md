35c2a1ca7c0dcd7ce0e31f6b4cd80287560290f4
Asfand Qazi <ayqazi@gmail.com>
2024-12-05 11:11:00 +0000

    Integration test added for bin/receipt, uses dummy code to pass
    Also, add thoughts on testing

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SUBSCRIBE Tech Test
+
+# Thoughts on decisions
+
+## Non-DRY tests
+
+Tests by their nature should be as simple as possible; the more complication there is in a test, the more chance of false positives now or in the future as the code evolves. So I personally prefer to keep tests on the repetitive side and not DRY too enthusiastically.
+
+It also makes them more readable as I can read a test and understand it without jumping back and forth between the test code and the custom matcher code.
+
+This is also a useful "test smell" - if large swathes of code are repeated in a test, it may indicate the need to factor out components.
--- spec/integration/receipt_integration_spec.rb
+++ spec/integration/receipt_integration_spec.rb
@@ -2,7 +2,7 @@
 
 require 'open3'
 
-RSpec.describe 'Receipt', 'integration' do
+RSpec.describe 'bin/receipt', 'integration' do
   def run(input)
     out, err, status = Open3.capture3("#{APP_ROOT}/bin/receipt", stdin_data: input)
     { out:, err:, status: }
@@ -13,5 +13,65 @@ RSpec.describe 'Receipt', 'integration' do
     expect(result[:status]).to be_a_success
     expect(result[:out]).not_to be_empty
     expect(result[:err]).to be_empty
+  end
+
+  it 'produces correct output for "Input 1"', :aggregate_failures do
+    input = <<~EOL
+      2 book at 12.49
+      1 music CD at 14.99
+      1 chocolate bar at 0.85
+    EOL
+
+    result = run(input)
+
+    expect(result[:status]).to be_a_success
+    expect(result[:err]).to be_empty
+    expect(result[:out]).to eq <<~EOL
+      2 book: 24.98
+      1 music CD: 16.49
+      1 chocolate bar: 0.85
+      Sales Taxes: 1.50
+      Total: 42.32
+    EOL
+  end
+
+  it 'produces correct output for "Input 2"', :aggregate_failures do
+    input = <<~EOL
+      1 imported box of chocolates at 10.00
+      1 imported bottle of perfume at 47.50
+    EOL
+
+    result = run(input)
+
+    expect(result[:status]).to be_a_success
+    expect(result[:err]).to be_empty
+    expect(result[:out]).to eq <<~EOL
+      1 imported box of chocolates: 10.50
+      1 imported bottle of perfume: 54.65
+      Sales Taxes: 7.65
+      Total: 65.15
+    EOL
+  end
+
+  it 'produces correct output for "Input 3"', :aggregate_failures do
+    input = <<~EOL
+      1 imported bottle of perfume at 27.99
+      1 bottle of perfume at 18.99
+      1 packet of headache pills at 9.75
+      3 imported boxes of chocolates at 11.25
+    EOL
+
+    result = run(input)
+
+    expect(result[:status]).to be_a_success
+    expect(result[:err]).to be_empty
+    expect(result[:out]).to eq <<~EOL
+      1 imported bottle of perfume: 32.19
+      1 bottle of perfume: 20.89
+      1 packet of headache pills: 9.75
+      3 imported box of chocolates: 35.55
+      Sales Taxes: 7.90
+      Total: 98.38
+    EOL
   end
 end
